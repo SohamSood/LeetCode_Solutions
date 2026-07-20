d767a1a64842286863faf8b6d0b1c11cb7cf3775
Soham Sood <soodsoham86@gmail.com>
2026-07-20 12:41:25 +0530

    Time: 20 ms (49.08%), Space: 36.1 MB (54.77%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         vector<vector<int>> adj(n);
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
         for(int i = 0;i<roads.size();i++) {
             adj[roads[i][0]].push_back(roads[i][1]);
             adj[roads[i][1]].push_back(roads[i][0]);
+            dp[roads[i][0]][roads[i][1]] = true;
+            dp[roads[i][1]][roads[i][0]] = true;
         }
         int maxcount = 0;
         for(int i = 0;i<adj.size();i++) {
-            unordered_set<int> freq;
-            for(int x:adj[i]) freq.insert(x);
-            int count = freq.size();
+            int count = adj[i].size();
             for(int j = i+1;j<adj.size();j++) {
-                if(freq.find(j) != freq.end()) {
+                if(dp[i][j]) {
                     count--;
                 }
                 count+=adj[j].size();
                 maxcount = max(maxcount,count);
-                count = freq.size();
+                count = adj[i].size();
             }
-            maxcount = max(maxcount,count);
         }
         return maxcount;
     }
