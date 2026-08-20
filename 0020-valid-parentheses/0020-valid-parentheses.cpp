7422cc7a4bee44d4714cf225ee18614ea1572c0a
Soham Sood <soodsoham86@gmail.com>
2026-08-20 14:25:35 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.74%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char x:s) {
+            if(st.empty()) st.push(x);
+            else{
+                if(st.top() == '(' && x == ')') st.pop();
+                else if(st.top() == '{' && x == '}') st.pop();
+                else if(st.top() == '[' && x == ']') st.pop();
+                else st.push(x);
+            }
+        }
+        return st.empty();
+    }
+};
