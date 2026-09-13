9c2c2e5f8a4894e872003e98f415a904f01a4d13
Soham Sood <soodsoham86@gmail.com>
2026-09-13 13:01:43 +0530

    Time: 0 ms (100%), Space: 27.4 MB (21.7%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
-        vector<int> dp(arr.size()); //with deletion
-        vector<int> dp2(arr.size()); //without deletion
-        dp2[0] = arr[0];
-        dp[0] = arr[0];
+        int without_deletion = arr[0];//without deletion
+        int with_deletion = arr[0];//with deletion
         int ans = arr[0];
         for(int i = 1;i<arr.size();i++) {
-            dp2[i] = max(arr[i],dp2[i-1] + arr[i]); // previous undeleted max(array starts from this element , previous undeleted + currentleement);
-            dp[i] = max(dp2[i-1],dp[i-1]+arr[i]); // deleted max(current element is deleted , or previously any element was deleted we are simply continuing)
-            ans = max(ans, max(dp[i], dp2[i]));
+            with_deletion = max(without_deletion,with_deletion+arr[i]); // deleted max(current element is deleted , or previously any element was deleted we are simply continuing)
+            without_deletion = max(arr[i],without_deletion + arr[i]); // previous undeleted max(array starts from this element , previous undeleted + currentleement);
+            ans = max(ans, max(with_deletion, without_deletion));
         }
-        // for(int c:dp) cout<<c<<" ";
+        // for(int c:with_deletion) cout<<c<<" ";
         // cout<<endl;
-        // for(int c:dp2) cout<<c<<" "; 
+        // for(int c:without_deletion) cout<<c<<" "; 
         return ans;
 
     }
