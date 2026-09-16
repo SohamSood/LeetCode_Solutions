1882663c372036c43eb8a1a22ccb5ed95932de7b
Soham Sood <soodsoham86@gmail.com>
2026-09-16 19:46:41 +0530

    Time: 27 ms (40.12%), Space: 59.7 MB (90.67%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canRun(int n,long long time,vector<int>& batteries) {
+        long long req_time = 0;
+        for(int i = 0;i<batteries.size();i++){
+            req_time += min<long long>(batteries[i],time);
+        } 
+        return (time * 1LL * n) <= req_time; 
+    }
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long sum = 0;
+        for(int x: batteries) sum+=x;
+        long long left = 0;
+        long long right = sum / n;
+        long long ans = 0;
+        while(left <= right) {
+            long long mid = left+(right-left)/2;
+            if(canRun(n,mid,batteries)) {
+                left = mid+1;
+                ans = mid;
+            } else  {
+                right = mid-1;
+            }
+        }
+        return ans;
+    }
+};
